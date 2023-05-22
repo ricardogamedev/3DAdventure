06dd4c6744ef64731907fdd4f023c30554b39bea
Ricardo <90054781+ricardogamedev@users.noreply.github.com>
2023-05-22 15:39:48 -0300

    State Machine

---
+++ Assets/Scripts/Project/StateMachine/StateMachine.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+
+namespace Ebac.StateMachine
+{
+    public class StateMachine<T> where T : System.Enum
+    {
+        public Dictionary<T, StateBase> dictionaryState;
+
+        private StateBase _currentState;
+
+        public float timeToStartGame = 1f;
+
+        public StateBase CurrentState
+        {
+            get { return _currentState; }
+        }
+
+   
+        public void Init()
+        {
+            dictionaryState = new Dictionary<T, StateBase>();
+           
+        }
+
+
+        public void RegisterStates(T typeEnum, StateBase state)
+        {
+            dictionaryState.Add(typeEnum, state);
+        }
+
+
+        [Button]
+        public void SwitchState(T state)
+        {
+            if (_currentState != null) _currentState.OnStateExit();
+
+            _currentState = dictionaryState[state];
+
+           // if (_currentState != null) 
+                _currentState.OnStateEnter();
+
+        }
+
+        public void Update()
+        {
+            if (_currentState != null) _currentState.OnStateStay();
+
+        }
+
+    }
+}
